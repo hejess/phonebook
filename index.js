674487dee18cbc60e751661df75dea51b21fe264
Jessie He <jessie.he@deakin.edu.au>
2023-09-19 17:19:07 +1000

    phonebook api backend in Node.js and Express 3.8

--- index.js
+++ index.js
@@ -1,36 +1,107 @@
 const express = require('express')
 
 const app = express()
+// middleware to handle request and response objects
+//json-parse takes the raw data from the request,
+// parse it into a JavaScript object
+// and assigns it to the request as a new proprty body.
 app.use(express.json())
+const morgan = require('morgan')
+morgan.token('body', (req, res) => {
+    return JSON.stringify(req.body)
+})
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345'
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122'
+  }
 ]
 
-
-
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const currentDate = new Date()
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZoneName: 'short'
+  }
+  const formattedDate = currentDate.toLocaleString('en-AU', options)
+  response.send(
+    `<div>
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${formattedDate}</p>
+    </div>`
+  )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(note => note.id === id)
+  if (!person) {
+    response.status(404).end()
+  } else {
+    response.json(person)
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map(person => person.id)) : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(409).json({
+      error: `Person ${body.name} already exists`
+    })
+  }
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+  persons = persons.concat(person)
+  response.json(person)
+})
 const PORT = 3000
 app.listen(PORT)
